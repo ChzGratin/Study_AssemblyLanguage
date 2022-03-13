300517f486ce0d073da6814de616f77d95f5e768
ChzGratin <jounghyeonpark@gmail.com>
2022-03-13 16:14:01 +0900

    Ch04 Finished

--- x64/ChzOS/00.bootloader/bootloader.asm
+++ x64/ChzOS/00.bootloader/bootloader.asm
@@ -2,11 +2,49 @@
 [bits 16]	;16 bit
 
 section .text
+	jmp 0x07C0:START	;cs = 0x07C0, jmp START
 
-jmp $		;while(1){}
+START:
+	mov ax, 0x07C0
+	mov ds, ax
+	mov ax, 0xB800	;VGA text buffer
+	mov es, ax
+	
+	;======== screen clear ========
+	mov si, 0
+	
+SCRCLEAR:
+	mov byte[es:si], 0
+	mov byte[es:si+1], 0x0007	;{background-color: black; color: white}
+	
+	add si, 2
+	
+	cmp si, 80*25*2
+	jl SCRCLEAR
+	
+	;======== put string   ========
+	mov si, 0
+	mov di, 0
+	
+PUTSTR:
+	mov cl, byte[MSG_HELLO+si]
+	cmp cl, 0
+	je ENDPUTSTR
+	
+	mov byte[es:di], cl
+	
+	add si, 1
+	add di, 2
+	
+	jmp PUTSTR
+ENDPUTSTR:
+	
+	jmp $	;while(1){}
+	
+MSG_HELLO: db "Hello, ChzOS!", 0
+	
+	times 510 - ($-$$) db 0
 
-times 510 - ($-$$) db 0
-
-;MBR signature (= dw 0xAA55)
-db 0x55
-db 0xAA
+	;MBR signature (= dw 0xAA55)
+	db 0x55
+	db 0xAA
